c0d7b302838a49f38ea1f944f04d2388383799fc
darina9 <krastinadv@gmail.com>
2022-09-01 17:22:53 +0300

    Zadacha_25

---
+++ Zadacha_25/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите число А и В:");
+
+
+int A = Convert.ToInt32(Console.ReadLine());
+int B = Convert.ToInt32(Console.ReadLine());
+
+
+{
+    Console.WriteLine("А в степени В = "+ Math.Pow(A,B));
+}
