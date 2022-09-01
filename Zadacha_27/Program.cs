bc7e2a1f87c02e442fcebbf66f496ad544c516fd
darina9 <krastinadv@gmail.com>
2022-09-01 20:56:40 +0300

    Zadacha_27

---
+++ Zadacha_27/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("введите число");
+int num = Convert.ToInt32(Console.ReadLine());
+int sum = 0;
+
+while (num > 0)
+{
+int i = num % 10;
+num = num / 10;
+sum += i;
+ }
+Console.WriteLine("сумма всех цифр в числе равна: " + sum);
