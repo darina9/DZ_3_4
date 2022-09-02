d9d13561daa2f80744be9a6fdeb626d429cc347d
darina9 <krastinadv@gmail.com>
2022-09-02 12:06:44 +0300

    Zadacha_29

---
+++ Zadacha_29/Program.cs
@@ -0,0 +1,11 @@
+﻿int[]array = GetArray(8);
+Console.WriteLine(string.Join("," , array));
+int[] GetArray(int size)
+{
+    int[] result = new int[size];
+    for(int i = 0;i<size;i++)
+    {
+        result [i] = new Random().Next(0,34);
+    }
+    return result;
+}
